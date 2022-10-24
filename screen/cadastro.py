4f7ba076432e6ff8f669b04819c5ef33ad06904b
GuiGolfeto <ggfguigofeto@gmail.com>
2022-10-24 07:17:41 -0300

    add new value

--- screen/cadastro.py
+++ screen/cadastro.py
@@ -9,7 +9,7 @@ class screenCadastroNF:
             [sg.Text('Nota Fiscal', size=(10,0)), sg.Input(size=(15,0), key='nf'),
             sg.Text('Frota', size=(10,0)), sg.Input(size=(15,0), key='frota')],
             [sg.Text('Fornecedor', size=(10,0)), sg.Input(size=(15,0), key='fornecedor'), 
-            sg.Text('Responsavel', size=(10,0)), sg.Input(size=(15,0), key='interessado')],
+            sg.Text('Responsavel', size=(10,0)), sg.Input(size=(15,0), key='responsavel')],
             [sg.Text('Qual Departamento?')],
             [sg.Radio('Saude', 'dep', key='saude'), sg.Radio('Educação', 'dep',  key='educação'), sg.Radio('Outros', 'dep', key='outros')],
             [sg.Text('Viagem?')],
@@ -30,14 +30,18 @@ class screenCadastroNF:
 
             if self.button == "Enviar dados":
                 nf = self.values['nf']
+                frota = self.values['frota']
                 fornecedor = self.values['fornecedor']
+                responsavel = self.values['responsavel']
                 saude = self.values['saude']
                 educação = self.values['educação']
                 outros = self.values['outros']
                 viagemSim = self.values['viagem']
                 viagemNao = self.values['naoViagem']
                 print(f'NF: {nf}')
-                print(f'Idade: {fornecedor}')
+                print(f'Frota: {frota}')
+                print(f'Fornecedor: {fornecedor}')
+                print(f'Responsavel: {responsavel}')
                 print(f'Saude: {saude}')
                 print(f'Educação: {educação}')
                 print(f'Outros: {outros}')
